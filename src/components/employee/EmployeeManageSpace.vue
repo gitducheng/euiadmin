b71cfc21121f76c0d1c44ed827880997df57d58b
gitducheng <1147914778@qq.com>
2021-01-14 09:51:51 +0800

    修改

--- src/components/employee/EmployeeManageSpace.vue
+++ src/components/employee/EmployeeManageSpace.vue
@@ -2,72 +2,96 @@
   <div>
     <el-row>
       <el-col :span="24">
-          <div>员工管理</div>
-    </el-col>
+        <div>员工管理</div>
+      </el-col>
     </el-row>
     <el-row>
-        <el-col :span="8">
-          <div><el-select v-model="value" placeholder="请选择">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select></div>
-        </el-col>
-        <el-col :span="8">
-          <div><el-select v-model="value" placeholder="请选择">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select></div>
-        </el-col>
-        <el-col :span="8">
-          <div><el-select v-model="value" placeholder="请选择">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select></div>
-        </el-col>
+      <el-col :span="8">
+        <label for="" class="emp_item_label">姓名：</label>
+        <div class="emp_item_content">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <label for="" class="emp_item_label">岗位：</label>
+        <div class="emp_item_content">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <label for="" class="emp_item_label">学历：</label>
+        <div class="emp_item_content">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
     </el-row>
     <el-row>
-        <el-col :span="8">
-          <div><el-select v-model="value" placeholder="请选择">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select></div>
-        </el-col>
-        <el-col :span="8">
-            <div><el-select v-model="value" placeholder="请选择">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select></div>
-        </el-col>
-        <el-col :span="8">
-          <div><el-select v-model="value" placeholder="请选择">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select></div>
-        </el-col>
+      <el-col :span="8">
+        <label for="" class="emp_item_label">职级：</label>
+        <div class="emp_item_content">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <label for="" class="emp_item_label">合同类型：</label>
+        <div class="emp_item_content">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <label for="" class="emp_item_label">在职状态：</label>
+        <div class="emp_item_content">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -75,29 +99,35 @@
 export default {
   data() {
     return {
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: ''
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      value: "",
     };
   },
 };
 </script>
 <style lang="scss" scoped>
 .el-row {
-    margin-top: 20px;
+  margin-top: 20px;
 }
 </style>
