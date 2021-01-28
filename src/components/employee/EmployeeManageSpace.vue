a7c521d64ccbd2ec9e77b76dbe7ffb72ee450aac
gitducheng <1147914778@qq.com>
2021-01-28 15:09:55 +0800

    add: employeemanage

--- src/components/employee/EmployeeManageSpace.vue
+++ src/components/employee/EmployeeManageSpace.vue
@@ -2,11 +2,11 @@
   <div>
     <el-row>
       <el-col :span="24">
-        <div>员工管理</div>
+        <div class="emp_title">员工管理</div>
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="8">
+      <el-col :span="8" class="emp_item emp_item_8">
         <label for="" class="emp_item_label">姓名：</label>
         <div class="emp_item_content">
           <el-select v-model="value" placeholder="请选择">
@@ -20,7 +20,7 @@
           </el-select>
         </div>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="8" class="emp_item emp_item_8">
         <label for="" class="emp_item_label">岗位：</label>
         <div class="emp_item_content">
           <el-select v-model="value" placeholder="请选择">
@@ -34,7 +34,7 @@
           </el-select>
         </div>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="8" class="emp_item emp_item_8">
         <label for="" class="emp_item_label">学历：</label>
         <div class="emp_item_content">
           <el-select v-model="value" placeholder="请选择">
@@ -50,7 +50,7 @@
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="8">
+      <el-col :span="8" class="emp_item emp_item_8">
         <label for="" class="emp_item_label">职级：</label>
         <div class="emp_item_content">
           <el-select v-model="value" placeholder="请选择">
@@ -64,7 +64,7 @@
           </el-select>
         </div>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="8" class="emp_item emp_item_8">
         <label for="" class="emp_item_label">合同类型：</label>
         <div class="emp_item_content">
           <el-select v-model="value" placeholder="请选择">
@@ -78,7 +78,7 @@
           </el-select>
         </div>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="8" class="emp_item emp_item_8">
         <label for="" class="emp_item_label">在职状态：</label>
         <div class="emp_item_content">
           <el-select v-model="value" placeholder="请选择">
@@ -90,6 +90,32 @@
             >
             </el-option>
           </el-select>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12" class="emp_item emp_item_12">
+        <label for="" class="emp_item_label">入职时间：</label>
+        <div class="emp_item_content">
+          <el-col :span="11">
+            <el-date-picker type="date" placeholder="选择日期" v-model="value" style="width: 100%;"></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker placeholder="选择时间" v-model="value" style="width: 100%;"></el-time-picker>
+          </el-col>
+        </div>
+      </el-col>
+      <el-col :span="12" class="emp_item emp_item_12">
+        <label for="" class="emp_item_label">离职时间：</label>
+        <div class="emp_item_content">
+          <el-col :span="11">
+            <el-date-picker type="date" placeholder="选择日期" v-model="value" style="width: 100%;"></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker placeholder="选择时间" v-model="value" style="width: 100%;"></el-time-picker>
+          </el-col>
         </div>
       </el-col>
     </el-row>
@@ -129,5 +155,33 @@ export default {
 <style lang="scss" scoped>
 .el-row {
   margin-top: 20px;
+}
+.emp {
+  &_title {
+    font-size: 20px;
+    border-bottom: 2px solid #ccc;
+  }
+
+  &_item {
+
+    &_8, &_12 {
+      padding-left: 10px;
+    }
+    
+    &_label {
+      float: left;
+      width: 80px;
+      text-align: right;
+      line-height: 40px;
+    }
+
+    &_content {
+      padding-left: 80px;
+    }
+  }
+}
+.line {
+  line-height: 40px;
+  text-align: center;
 }
 </style>
